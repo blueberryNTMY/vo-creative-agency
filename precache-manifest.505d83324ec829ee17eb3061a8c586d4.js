4a296b267671905bc052dd6fcb2beaa2fd30fea6
blueBerry <vlad.snouk@gmail.com>
2020-02-26 23:27:09 +0200

    Updates

---
+++ precache-manifest.505d83324ec829ee17eb3061a8c586d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2f8caadd4c70d415c6f7a141c4a9d4",
+    "url": "/vo-creative-agency/index.html"
+  },
+  {
+    "revision": "34fa51524ffa9fa82545",
+    "url": "/vo-creative-agency/static/css/main.c1173615.chunk.css"
+  },
+  {
+    "revision": "d58998bfa6d76c1bc4af",
+    "url": "/vo-creative-agency/static/js/2.5117cfee.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/vo-creative-agency/static/js/2.5117cfee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fa51524ffa9fa82545",
+    "url": "/vo-creative-agency/static/js/main.8f89041c.chunk.js"
+  },
+  {
+    "revision": "79d16e278d6e0e335fa4",
+    "url": "/vo-creative-agency/static/js/runtime-main.d47a83a5.js"
+  },
+  {
+    "revision": "548d6017ce493d15142ea07d57d25ac3",
+    "url": "/vo-creative-agency/static/media/comments.548d6017.svg"
+  },
+  {
+    "revision": "acb509dd6b186a7afa67e252191000e7",
+    "url": "/vo-creative-agency/static/media/site-hero.acb509dd.jpg"
+  },
+  {
+    "revision": "db44d046dc1900e67d770cd063588ed6",
+    "url": "/vo-creative-agency/static/media/site-hero2.db44d046.jpg"
+  }
+]);
